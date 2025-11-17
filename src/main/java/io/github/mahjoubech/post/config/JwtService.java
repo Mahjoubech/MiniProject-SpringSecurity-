868dc69e03725737fc788bb2cb265d98aa9228e4
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-11-17 02:24:35 +0100

    Implement JWT service for token generation and claim extraction

--- src/main/java/io/github/mahjoubech/post/config/JwtService.java
+++ src/main/java/io/github/mahjoubech/post/config/JwtService.java
@@ -1,10 +1,54 @@
 package io.github.mahjoubech.post.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
+    private static final  String SECRET_KEY = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B59703373";
     public String extractUserEmail(String jwt) {
-        return null;
+        return extractClaim(jwt, Claims::getSubject);
     }
+
+    public <T> T extractClaim(String jwt, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(jwt);
+        return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(Map<String,Object> claims ,
+    UserDetails userDetails) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(userDetails.getEmail())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSigningKey()  , SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Claims extractAllClaims(String jwt) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(jwt)
+                .getBody();
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
 }
